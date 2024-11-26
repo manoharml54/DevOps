fbd43d690f32f6ea2b5197003734a678d03d1c01
manoharml54 <manohar.ml54@gmail.com>
2024-11-26 09:03:25 +0000

    second script

---
+++ shellscript/aws_resource_tracker.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+
+##################
+#Author: Manohar
+#Date: 13th oct
+#
+#Version: v1
+#
+#This script will report the AWS resource usage
+###################
+
+set -x
+
+# AWS S3
+# AWS EC2
+# AWS Lambda
+# AWS IAM Users
+
+# list s3 buckets
+echo "Print list of s3 buckets"
+aws s3 ls 
+
+# list EC2 Instances
+echo "Print list of ec2 Instances"
+aws ec2 describe-instances | jq '.Reservations[].Instances[].InstanceId'
+
+# list Lambda
+echo "Print list of Lambda"
+aws lambda list-functions
+
+# list IAM Users
+echo "Print list of IAM Users"
+aws iam list-users
+
+
+## aws ec2 describe-instances | jq '.Reservations[].Instances[].InstanceId'
+
+
